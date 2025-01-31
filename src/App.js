f6362241c4de75f7deb7404e4ee6dd70bc6f591c
Ro706 <rohitmandal99091@gmail.com>
2025-01-31 11:47:03 +0530

    update poster

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import { SpeedInsights } from "@vercel/speed-insights/react"
 import React from 'react';
 import Navbar from './component/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
